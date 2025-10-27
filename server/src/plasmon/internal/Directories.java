e685d9a8a7d03eef0d95c75d33fe5bef295520e7
Alex Archambault <alexandre.archambault@gmail.com>
2025-10-27 19:08:56 +0100

    coursier-interface-no-shading in native image

--- server/src/plasmon/internal/Directories.java
+++ server/src/plasmon/internal/Directories.java
@@ -1,8 +1,8 @@
 package plasmon.internal;
 
-import coursierapi.shaded.coursier.cache.shaded.dirs.ProjectDirectories;
-import coursierapi.shaded.coursier.cache.shaded.dirs.impl.Windows;
-import coursierapi.shaded.coursier.cache.shaded.dirs.jni.WindowsJni;
+import coursier.paths.shaded.dirs.ProjectDirectories;
+import coursier.paths.shaded.dirs.impl.Windows;
+import coursier.paths.shaded.dirs.jni.WindowsJni;
 
 import java.util.function.Supplier;
 
